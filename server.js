1507d7be7f4640d1fa454bf8aa5e75ee39e672cf
Seu Nome <108072743+jadsonogueira@users.noreply.github.com>
2024-09-21 18:34:21 -0400

    Ajusta app.listen para ouvir em 0.0.0.0

--- server.js
+++ server.js
@@ -138,6 +138,6 @@ app.get('/', (req, res) => {
 
 // Iniciar o servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
